9c8d385287f67fdd43294ec46d55db0f1aef71b2
Pamela Wagatuma <pamelamm123@gmail.com>
2026-06-27 22:59:14 +0900

    alteracao do helper conforme revisor solicitou, os outros estavam prontos mas nao tinham sido feito commit

--- data.py
+++ data.py
@@ -1,1 +1,15 @@
 URBAN_ROUTES_URL = ("https://cnt-5bd4c2d5-1a60-41c0-bcbf-ff4ce66e489f.containerhub.tripleten-services.com/")
+
+# Arquivo com constantes
+
+ADDRESS_FROM = 'East 2nd Street, 601'
+
+ADDRESS_TO = '1300 1st St'
+
+PHONE_NUMBER = '+1 123 123 12 12'
+
+CARD_NUMBER = '1234 5678 9100'
+
+CARD_CODE = '1111'
+
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
